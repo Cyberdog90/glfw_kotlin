921bb929c62444be9e8245928f841d9fdc0e0229
CYBAiGNU <43635561+Cyberdog90@users.noreply.github.com>
2021-04-22 02:28:06 +0900

    p35

--- src/main/kotlin/ex.kt
+++ src/main/kotlin/ex.kt
@@ -1,2 +1,2 @@
 fun Boolean.toInt() = if (this) 1 else 0
-fun Int.toBoolean() = this == 0
+fun Int.toBoolean() = this != 0
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,22 @@
+import org.lwjgl.glfw.GLFW.glfwCreateWindow
 import org.lwjgl.glfw.GLFW.glfwInit
 import org.lwjgl.opengl.GL11.GL_FALSE
+import org.lwjgl.system.MemoryUtil.NULL
 import kotlin.system.exitProcess
 
 fun main() {
+    // GLFWを初期化する
     if (glfwInit() == GL_FALSE.toBoolean()) {
         // 初期化に失敗した
-        println("Can't initialize GLFW")
+        System.err.println("Can't initialize GLFW")
+        exitProcess(1)
+    }
+
+    // ウィンドウを作成する
+    val window: Long = glfwCreateWindow(640, 480, "Hello!", NULL, NULL)
+    if (window == NULL) {
+        // ウィンドウが作成できなかった
+        System.err.println("Can't create GLFW window.")
         exitProcess(1)
     }
 }
