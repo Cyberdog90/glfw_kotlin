5d8481d56c6ddab545428409deda8c05116fa000
CYBAiGNU <43635561+Cyberdog90@users.noreply.github.com>
2021-04-22 02:15:43 +0900

    p34

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,11 @@
-fun main() {
+import org.lwjgl.glfw.GLFW.glfwInit
+import org.lwjgl.opengl.GL11.GL_FALSE
+import kotlin.system.exitProcess
 
+fun main() {
+    if (glfwInit() == GL_FALSE.toBoolean()) {
+        // 初期化に失敗した
+        println("Can't initialize GLFW")
+        exitProcess(1)
+    }
 }
